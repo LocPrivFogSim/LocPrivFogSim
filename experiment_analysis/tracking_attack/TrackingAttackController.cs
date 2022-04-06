f88e038c71ff8db260464882c053887fcf87b436
Markus Schlotbohm <markus_schlotbohm@as-gmbh.de>
2022-04-06 12:36:18 +0200

    cleanup

--- experiment_analysis/tracking_attack/TrackingAttackController.cs
+++ experiment_analysis/tracking_attack/TrackingAttackController.cs
@@ -8,13 +8,13 @@ using Data;
 public class TrackingAttackController
 {
 
-    public Dictionary<int, Coord> _locations { get; set;}
+    public Dictionary<int, Coord> _locations { get; set; }
 
-    Dictionary<int, List<Coord>> _paths { get; set;}
+    Dictionary<int, List<Coord>> _paths { get; set; }
 
-    Dictionary<int, Device> _fogNodes {get; set;}
+    Dictionary<int, Device> _fogNodes { get; set; }
 
-    List<SquareField> _squareFields {get; set;}
+    List<SquareField> _squareFields { get; set; }
 
 
 
@@ -22,15 +22,15 @@ public class TrackingAttackController
     //Controller Method for tracking attack
     public async Task RunTrackingAttack()
     {
-        DB_Connector connector =  new DB_Connector();
-        _paths =  connector.GetAllPaths();
+        DB_Connector connector = new DB_Connector();
+        _paths = connector.GetAllPaths();
 
         JsonParser jsonParser = new JsonParser();
         _locations = jsonParser.GetLocations();
         _fogNodes = jsonParser.InitFogNodes();
         _squareFields = jsonParser.GetSquareFields(_locations, _fogNodes);
 
-        var files = Directory.GetFiles(Constants.EventsFilesDir, "*.json" , SearchOption.AllDirectories);
+        var files = Directory.GetFiles(Constants.EventsFilesDir, "*.json", SearchOption.AllDirectories);
         var results = ArrayList.Synchronized(new ArrayList(files.Length));
 
         Parallel.ForEach(files, (file, _, _) =>
@@ -46,7 +46,7 @@ public class TrackingAttackController
     {
         Console.WriteLine(filepath);
 
-        int [] experimentParams = getExperimentParamsFromFileName(filepath);
+        int[] experimentParams = getExperimentParamsFromFileName(filepath);
         int strategy = experimentParams[0];
         int rate = experimentParams[1];
         int iteration = experimentParams[2];
@@ -73,7 +73,7 @@ public class TrackingAttackController
 
 
 
-    private AttackResult calcClosest(Dictionary<int, Coord> locations, EventFileData eventFileData, Dictionary<int, List<Coord>> paths, Dictionary<int, Device> fogNodes )
+    private AttackResult calcClosest(Dictionary<int, Coord> locations, EventFileData eventFileData, Dictionary<int, List<Coord>> paths, Dictionary<int, Device> fogNodes)
     {
         //TODO
 
@@ -81,12 +81,12 @@ public class TrackingAttackController
 
         Dictionary<int, double> pathProbablity = new Dictionary<int, double>();
 
-        for(int i = 0 ; i < paths.Count(); i++)
+        for (int i = 0; i < paths.Count(); i++)
         {
             double alpha = 0;
             List<Coord> path = paths[i];
 
-            Dictionary<int, Segment> segments =  Calculations.SampleSegments(path);
+            Dictionary<int, Segment> segments = Calculations.SampleSegments(path);
 
         }
 
@@ -95,7 +95,7 @@ public class TrackingAttackController
         return result;
     }
 
-    private AttackResult calcFastest(Dictionary<int, Coord> locations, EventFileData eventFileData, Dictionary<int, List<Coord>> paths, Dictionary<int, Device> fogNodes, Dictionary<int, DeviceStats> fogDeviceStats )
+    private AttackResult calcFastest(Dictionary<int, Coord> locations, EventFileData eventFileData, Dictionary<int, List<Coord>> paths, Dictionary<int, Device> fogNodes, Dictionary<int, DeviceStats> fogDeviceStats)
     {
 
         AttackResult result = new AttackResult();
@@ -104,10 +104,10 @@ public class TrackingAttackController
 
         List<Event> events = eventFileData.Events;
 
-   
+
         //for(int pathID = 0 ; pathID < paths.Count(); pathID++)
         //for(int pathID = 25381 -50 ; pathID < 25381 + 50; pathID++)
-        for(int pathID = 25381; pathID < 25381 +1 ; pathID++)
+        for (int pathID = 25381; pathID < 25381 + 1; pathID++)
         {
             //heuristic filter to improve perfomance
             List<Coord> path = _paths[pathID];
@@ -115,37 +115,37 @@ public class TrackingAttackController
             Coord a_last = path.Last();
             Coord b_first = _paths[eventFileData.SimulatedPathId][0];
             Coord b_last = _paths[eventFileData.SimulatedPathId].Last();
-            if(Calculations.CalcDistanceInMetres(a_first,b_first) > 10000 ) continue;
+            if (Calculations.CalcDistanceInMetres(a_first, b_first) > 10000) continue;
 
             double bearingA = Calculations.Bearing(a_first, a_last);
             double bearingB = Calculations.Bearing(b_first, b_last);
             double bearingDelta = Math.Abs(bearingA - bearingB);
 
-            if(bearingDelta > 30) continue;
-
-            
+            if (bearingDelta > 30) continue;
 
 
-            Console.WriteLine("path: "+pathID);
+
+
+            Console.WriteLine("path: " + pathID);
 
 
             double alpha = 0;
-            Dictionary<int, Segment> segments =  Calculations.SampleSegments(path);
+            Dictionary<int, Segment> segments = Calculations.SampleSegments(path);
 
             int nrOfValidSegmentations = 0;
 
-            for(int j  = 0 ; j < Constants.NumberOfIterations; j++)
+            for (int j = 0; j < Constants.NumberOfIterations; j++)
             {
 
                 segments = Calculations.SampleSegmentVelocities(segments);
 
-                double sumTraversingTime = calcSumTraversingTime(segments) ;
-                double lastTrackedTS = events[events.Count -1 ].Timestamp;
+                double sumTraversingTime = calcSumTraversingTime(segments);
+                double lastTrackedTS = events[events.Count - 1].Timestamp;
                 double trackedDuration = lastTrackedTS - events[0].Timestamp;
 
-                if(sumTraversingTime < trackedDuration) continue;
+                if (sumTraversingTime < trackedDuration) continue;
 
-                nrOfValidSegmentations ++;
+                nrOfValidSegmentations++;
 
                 double beta = 0;
 
@@ -156,9 +156,10 @@ public class TrackingAttackController
                 Dictionary<double, Coord> segmentCoordsAtTimestamps = Calculations.mapSegmentCoordsToTimestamps(segments, events, randStartTime);
 
 
-                for(int eventIndex = 0 ; eventIndex < events.Count/2 ; eventIndex++){
+                for (int eventIndex = 0; eventIndex < events.Count / 2; eventIndex++)
+                {
                     Event addEvent = events[eventIndex * 2];
-                    Event removeEvent = events[ eventIndex * 2 + 1];
+                    Event removeEvent = events[eventIndex * 2 + 1];
 
                     Coord guessedLocation = segmentCoordsAtTimestamps[addEvent.Timestamp];
                     Coord actualPos = getActualCoordOnPath(_paths[eventFileData.SimulatedPathId], addEvent.Timestamp);
@@ -166,13 +167,14 @@ public class TrackingAttackController
                     Device device = _fogNodes[addEvent.FogDeviceId];
 
 
-                    if (!Calculations.CoordIsInPolygon(guessedLocation, addEvent.ConsideredField)){
+                    if (!Calculations.CoordIsInPolygon(guessedLocation, addEvent.ConsideredField))
+                    {
                         continue;
                     }
 
                     int chosenDeviceId = fastestRespondingDeviceId(guessedLocation, addEvent, removeEvent, eventFileData, fogDeviceStats);
 
-                    if(chosenDeviceId != device.Id)
+                    if (chosenDeviceId != device.Id)
                     {
                         continue;
                     }
@@ -182,34 +184,35 @@ public class TrackingAttackController
 
                     List<Coord> relevantLocations = new List<Coord>();
 
-                    foreach(SquareField sf in _squareFields)
+                    foreach (SquareField sf in _squareFields)
                     {
-                        if(sf.fogNodeIDs.Contains(device.Id)){
+                        if (sf.fogNodeIDs.Contains(device.Id))
+                        {
                             relevantLocations = sf.locations;
                             break;
                         }
                     }
 
 
-                    foreach( Coord l in relevantLocations)
-               
+                    foreach (Coord l in relevantLocations)
+
                     {
-                        
+
                         chosenDeviceId = fastestRespondingDeviceId(l, addEvent, removeEvent, eventFileData, fogDeviceStats);
 
-                        if(chosenDeviceId == device.Id)
+                        if (chosenDeviceId == device.Id)
                         {
-                            countOfOtherPossibleLocations ++;
+                            countOfOtherPossibleLocations++;
                         }
 
                     }
-                   
 
-                    if(countOfOtherPossibleLocations > 0)
+
+                    if (countOfOtherPossibleLocations > 0)
                     {
-                       beta += (1 / countOfOtherPossibleLocations);          
+                        beta += (1 / countOfOtherPossibleLocations);
                     }
-                
+
 
                 }
 
@@ -217,7 +220,7 @@ public class TrackingAttackController
 
             }
 
-            if(alpha > 0)
+            if (alpha > 0)
             {
                 alpha = alpha / nrOfValidSegmentations;
                 pathProbablity[pathID] = alpha;
@@ -227,18 +230,18 @@ public class TrackingAttackController
         return result;
     }
 
-      private AttackResult calcNotSlow(Dictionary<int, Coord> locations, EventFileData eventFileData, Dictionary<int, List<Coord>> paths, Dictionary<int, Device> fogNodes, Dictionary<int, DeviceStats> fogDeviceStats )
+    private AttackResult calcNotSlow(Dictionary<int, Coord> locations, EventFileData eventFileData, Dictionary<int, List<Coord>> paths, Dictionary<int, Device> fogNodes, Dictionary<int, DeviceStats> fogDeviceStats)
     {
         //TODO
         AttackResult result = new AttackResult();
 
         Dictionary<int, double> pathProbablity = new Dictionary<int, double>();
 
-        for(int i = 0 ; i < paths.Count(); i++)
+        for (int i = 0; i < paths.Count(); i++)
         {
             double alpha = 0;
             List<Coord> path = paths[i];
-            Dictionary<int, Segment> segments =  Calculations.SampleSegments(path);
+            Dictionary<int, Segment> segments = Calculations.SampleSegments(path);
 
         }
 
@@ -260,13 +263,13 @@ public class TrackingAttackController
             double uplinkLatency = ev.FogDeviceInfos[x][2];
 
 
-           DeviceStats ds = new DeviceStats();
-           ds.IsCompromised = isCompromised;
-           ds.DownlinkBandwidth = downlinkBandwidth;
-           ds.UplinkBandwidth = uplinkBandwidth;
-           ds.UplinkLatency = uplinkLatency;
+            DeviceStats ds = new DeviceStats();
+            ds.IsCompromised = isCompromised;
+            ds.DownlinkBandwidth = downlinkBandwidth;
+            ds.UplinkBandwidth = uplinkBandwidth;
+            ds.UplinkLatency = uplinkLatency;
 
-           deviceStats[x] = ds;
+            deviceStats[x] = ds;
         }
         return deviceStats;
     }
@@ -283,7 +286,7 @@ public class TrackingAttackController
         int rate = int.Parse(fileNameSubStings[2]);
         int iteration = int.Parse(fileNameSubStings[3]);
 
-        return new int[] {strategy, rate, iteration};
+        return new int[] { strategy, rate, iteration };
     }
 
 
@@ -291,7 +294,8 @@ public class TrackingAttackController
     {
         double counter = 0;
 
-        foreach(Segment s in segments.Values){
+        foreach (Segment s in segments.Values)
+        {
             counter += s.TraversingTime;
         }
 
@@ -302,18 +306,20 @@ public class TrackingAttackController
     {
         return getActualCoordOnPath(path, timestamp, 0, path.Count);
     }
-    public Coord getActualCoordOnPath(List<Coord> path, double timestamp, int start, int end){
+    public Coord getActualCoordOnPath(List<Coord> path, double timestamp, int start, int end)
+    {
 
-        if( start > end ){
+        if (start > end)
+        {
             throw new Exception("getActualCoordOnPath(): no Coordinate with correct timestamp found");
         }
 
-        double x = (double) ((start +  end ) / 2);
-        int middle = (int) Math.Floor( x);
+        double x = (double)((start + end) / 2);
+        int middle = (int)Math.Floor(x);
 
-        if(path[middle].Timestamp == timestamp) return path[middle];
+        if (path[middle].Timestamp == timestamp) return path[middle];
 
-        if(path[middle].Timestamp > timestamp)
+        if (path[middle].Timestamp > timestamp)
         {
             return getActualCoordOnPath(path, timestamp, start, middle - 1);
         }
@@ -323,13 +329,13 @@ public class TrackingAttackController
 
     }
 
-    public int fastestRespondingDeviceId(Coord guessedLoc, Event addE, Event removeE, EventFileData eFileData,  Dictionary<int, DeviceStats> stats)
+    public int fastestRespondingDeviceId(Coord guessedLoc, Event addE, Event removeE, EventFileData eFileData, Dictionary<int, DeviceStats> stats)
     {
         int[] consideredDeviceIDs = addE.ConsideredFogNodes;
         int fastestNodeId = 0;
         double minRt = Double.MaxValue;
 
-         Dictionary<int, double> tmp = new Dictionary<int, double>(); //TODO Remove
+        Dictionary<int, double> tmp = new Dictionary<int, double>(); //TODO Remove
 
 
         foreach (int deviceID in consideredDeviceIDs)
@@ -339,7 +345,7 @@ public class TrackingAttackController
 
             tmp[d.Id] = rt; //TODO remove
 
-            if(rt < minRt)
+            if (rt < minRt)
             {
                 fastestNodeId = deviceID;
                 minRt = rt;
@@ -352,20 +358,21 @@ public class TrackingAttackController
 
     //---------- -- Singleton --------------------
 
-    private TrackingAttackController(){
+    private TrackingAttackController()
+    {
     }
 
     private static TrackingAttackController _instance = new TrackingAttackController();
 
-      public static TrackingAttackController Instance
+    public static TrackingAttackController Instance
+    {
+        get
         {
-            get
-            {
-                if (_instance == null)
-                    _instance = new TrackingAttackController();
-                return _instance;
-            }
+            if (_instance == null)
+                _instance = new TrackingAttackController();
+            return _instance;
         }
+    }
 
 }
 
@@ -373,11 +380,11 @@ public class TrackingAttackController
 
 class AttackResult
 {
-    public int strat {get;set;}
-    public int rate {get;set;}
-    public int iteration {get;set;}
-    public double corrFullDtwDist {get;set;}
-    public double corrAvgDtwDist {get; set;}
-    public int nrOfObservations {get;set;}
+    public int strat { get; set; }
+    public int rate { get; set; }
+    public int iteration { get; set; }
+    public double corrFullDtwDist { get; set; }
+    public double corrAvgDtwDist { get; set; }
+    public int nrOfObservations { get; set; }
 
 }
