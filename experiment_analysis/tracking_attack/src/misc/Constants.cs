cf5c0a98feb9c21548a15dd93101ce6e65c02276
snluspie <lukas.spiekermann@stud.uni-due.de>
2022-04-06 11:57:44 +0200

    to relative paths

--- experiment_analysis/tracking_attack/src/misc/Constants.cs
+++ experiment_analysis/tracking_attack/src/misc/Constants.cs
@@ -3,16 +3,16 @@ public static class Constants
 
     public const double MaxDistance = 14143; 
 
-    public const String LocationsFilePath =@"C:\Users\lspie\Desktop\LocPrivFogSim\experiment_analysis\json\locations_points.json";
+    public const String LocationsFilePath =@"..\json\locations_points.json";
 
-    public const String NodeLocationsFilePath =@"C:\Users\lspie\Desktop\LocPrivFogSim\experiment_analysis\json\locs_in_voronoy_for_node.json";
+    public const String NodeLocationsFilePath =@"..\json\locs_in_voronoy_for_node.json";
     
-    public const String SquaresFilePath = @"C:\Users\lspie\Desktop\LocPrivFogSim\experiment_analysis\json\10x10_squares.json";
+    public const String SquaresFilePath = @"..\json\10x10_squares.json";
     public const String ResultsFilePath = "./.."; 
 
-    public const String  EventsFilesDir= @"C:\Users\lspie\Desktop\LocPrivFogSim\experiment_analysis\input"; 
+    public const String  EventsFilesDir= @"..\input"; 
 
-    public const String DbPath = @"C:\Users\lspie\Desktop\LocPrivFogSim\geoLifePaths.db";
+    public const String DbPath = @"..\..\geoLifePaths.db";
 
     public const int LenOfSegments = 25; //25 metres
 
